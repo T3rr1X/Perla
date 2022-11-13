6f1789af5cc12cf4929a300cd0ee377e4b64caa9
T3rr1X <79317185+T3rr1X@users.noreply.github.com>
2022-11-13 11:25:41 +0100

    Create ADAM_View_2.0.py

---
+++ ADAM_View_2.0.py
@@ -0,0 +1,242 @@
+""" ADAM Visions """
+import ADAM as ad
+import streamlit as stl
+import matplotlib.pyplot as plt
+import yfinance
+from plotly import graph_objects as go
+from datetime import date
+import pandas as pd
+
+if True:
+    def SuperTrend(df, lb=14, mlt=3.8):
+        import numpy as np
+
+        def get_supertrend(high, low, close, lookback, multiplier):
+            # ATR
+            tr1 = pd.DataFrame(high - low)
+            tr2 = pd.DataFrame(abs(high - close.shift(1)))
+            tr3 = pd.DataFrame(abs(low - close.shift(1)))
+            frames = [tr1, tr2, tr3]
+            tr = pd.concat(frames, axis=1, join='inner').max(axis=1)
+            atr = tr.ewm(lookback).mean()
+            # H/L AVG AND BASIC UPPER & LOWER BAND
+            hl_avg = (high + low) / 2
+            upper_band = (hl_avg + multiplier * atr).dropna()
+            lower_band = (hl_avg - multiplier * atr).dropna()
+            # FINAL UPPER BAND
+            final_bands = pd.DataFrame(columns=['upper', 'lower'])
+            final_bands.iloc[:, 0] = [x for x in upper_band - upper_band]
+            final_bands.iloc[:, 1] = final_bands.iloc[:, 0]
+            for i in range(len(final_bands)):
+                if i == 0:
+                    final_bands.iloc[i, 0] = 0
+                else:
+                    if (upper_band[i] < final_bands.iloc[i - 1, 0]) | (close[i - 1] > final_bands.iloc[i - 1, 0]):
+                        final_bands.iloc[i, 0] = upper_band[i]
+                    else:
+                        final_bands.iloc[i, 0] = final_bands.iloc[i - 1, 0]
+            # FINAL LOWER BAND
+            for i in range(len(final_bands)):
+                if i == 0:
+                    final_bands.iloc[i, 1] = 0
+                else:
+                    if (lower_band[i] > final_bands.iloc[i - 1, 1]) | (close[i - 1] < final_bands.iloc[i - 1, 1]):
+                        final_bands.iloc[i, 1] = lower_band[i]
+                    else:
+                        final_bands.iloc[i, 1] = final_bands.iloc[i - 1, 1]
+            # SUPERTREND
+            supertrend = pd.DataFrame(columns=[f'supertrend_{lookback}'])
+            supertrend.iloc[:, 0] = [x for x in final_bands['upper'] - final_bands['upper']]
+            for i in range(len(supertrend)):
+                if i == 0:
+                    supertrend.iloc[i, 0] = 0
+                elif supertrend.iloc[i - 1, 0] == final_bands.iloc[i - 1, 0] and close[i] < final_bands.iloc[i, 0]:
+                    supertrend.iloc[i, 0] = final_bands.iloc[i, 0]
+                elif supertrend.iloc[i - 1, 0] == final_bands.iloc[i - 1, 0] and close[i] > final_bands.iloc[i, 0]:
+                    supertrend.iloc[i, 0] = final_bands.iloc[i, 1]
+                elif supertrend.iloc[i - 1, 0] == final_bands.iloc[i - 1, 1] and close[i] > final_bands.iloc[i, 1]:
+                    supertrend.iloc[i, 0] = final_bands.iloc[i, 1]
+                elif supertrend.iloc[i - 1, 0] == final_bands.iloc[i - 1, 1] and close[i] < final_bands.iloc[i, 1]:
+                    supertrend.iloc[i, 0] = final_bands.iloc[i, 0]
+            supertrend = supertrend.set_index(upper_band.index)
+            supertrend = supertrend.dropna()[1:]
+            # ST UPTREND/DOWNTREND
+            upt = []
+            dt = []
+            close = close.iloc[len(close) - len(supertrend):]
+            for i in range(len(supertrend)):
+
+                if close[i] > supertrend.iloc[i, 0]:
+                    upt.append(supertrend.iloc[i, 0])
+                    dt.append(np.nan)
+                elif close[i] < supertrend.iloc[i, 0]:
+                    upt.append(np.nan)
+                    dt.append(supertrend.iloc[i, 0])
+                else:
+                    upt.append(np.nan)
+                    dt.append(np.nan)
+            st, upt, dt = pd.Series(supertrend.iloc[:, 0]), pd.Series(upt), pd.Series(dt)
+            upt.index, dt.index = supertrend.index, supertrend.index
+            return st, upt, dt
+
+        def implement_st_strategy(prices, st):
+            buy_price = []
+            sell_price = []
+            st_signal = []
+            signal = 0
+
+            for i in range(len(st)):
+                if st[i - 1] > prices[i - 1] and st[i] < prices[i]:
+                    if signal != 1:
+                        buy_price.append(prices[i])
+                        sell_price.append(np.nan)
+                        signal = 1
+                        st_signal.append(signal)
+                    else:
+                        buy_price.append(np.nan)
+                        sell_price.append(np.nan)
+                        st_signal.append(0)
+                elif st[i - 1] < prices[i - 1] and st[i] > prices[i]:
+                    if signal != -1:
+                        buy_price.append(np.nan)
+                        sell_price.append(prices[i])
+                        signal = -1
+                        st_signal.append(signal)
+                    else:
+                        buy_price.append(np.nan)
+                        sell_price.append(np.nan)
+                        st_signal.append(0)
+                else:
+                    buy_price.append(np.nan)
+                    sell_price.append(np.nan)
+                    st_signal.append(0)
+
+            return buy_price, sell_price, st_signal
+
+        print(df['Close'])
+        df_st = get_supertrend(df['High'], df['Low'], df['Close'], lookback=lb, multiplier=mlt)
+        # print(df_st)
+
+        buy_price, sell_price, st_signal = implement_st_strategy(df['Close'], df_st[0])
+
+        # print(buy_price, sell_price, st_signal)
+        return df_st, buy_price, sell_price, st_signal
+
+START = "2022-10-01"
+END = date.today().strftime("%Y-%m-%d")
+
+stl.header('Perla Enviroment ADAM View 2.0')
+stock = str(stl.text_input('Stock'))
+time_period = str(stl.text_input('TimeFrame (5m, 15m, 1d, 1mo)'))
+tipe = str(stl.text_input('Time Period (1d, 1w, 1mo)'))
+
+dffstart = yfinance.download(tickers=stock, start=START, end=END,  interval=time_period)
+df = dffstart.Close
+dff = dffstart
+dff.reset_index(inplace=True)
+
+showframe = stl.checkbox('Show DataFrame?')
+if showframe:
+    stl.subheader('Data Frame')
+    stl.dataframe(dff)
+
+Filtring = stl.checkbox('Usare Filtro?')
+MA1se = stl.checkbox('ad MA 1?')
+MA2se = stl.checkbox('ad MA 2?')
+Stse = stl.checkbox("Super Trend?")
+if Stse:
+    EtichetteST = stl.checkbox('Etichette ST?')
+    dffull = ad.incredible_data_requestor_full(stock, time_period, 1, pr=tipe)
+ATRse = stl.checkbox("double ATR?")
+TMAse = stl.checkbox("TMAs?")
+if MA1se:
+    MA1_Lenght = int(stl.slider("adMA short Lenght", 1, 200))
+    MA1 = ad.MA(df, MA1_Lenght)[::-1]
+if MA2se:
+    MA2_Lenght = int(stl.slider("adMA2 long Lenght", 1, 200))
+    MA2 = ad.MA(df, MA2_Lenght)[::-1]
+if Stse:
+    ST_M = int(stl.slider("ST period", 1, 60))
+    ST_P = float(stl.slider("ST Multiplier", 1.0, 6.0))
+    df_st, buy_price, sell_price, st_signal = SuperTrend(dffull, ST_P, ST_M)
+if TMAse:
+    TMAs_Lenght = int(stl.slider('TMA Lenght', 1, 200))
+    TMA = ad.TMA(df, TMAs_Lenght)
+    TMA = list(TMA)
+    TMADOWN = []
+    TMAUP = []
+    ATR_PERIOD = 120
+    ATR_MULT = 1.8
+    atr = ad.ATR(dff, ATR_PERIOD)
+    rangee = []
+    for i in range(len(atr)):
+        rangee.append(atr[i] * ATR_MULT)
+
+    for i in range(len(TMA)):
+        TMADOWN.append(TMA[i] - rangee[i])
+        TMAUP.append(TMA[i] + rangee[i])
+
+stl.subheader('Plot')
+stl.write('Puoi ingrandire il grafico')
+
+if Filtring:
+    MA3_Lenght = int(stl.slider("adMA filter Lenght", 1, 200))
+    MA3 = ad.MA(df, MA3_Lenght)[::-1]
+
+if ATRse:
+    x1 = ad.ATR_SL(dff, 9)
+    x2 = ad.ATR_SL(dff, 9)
+    xx1 = []
+    xx2 = []
+    for i in range(len(x1)):
+        xx1.append((x1[i] * 1.5) + dff['High'][i])
+        xx2.append(dff['Low'][i] - (x1[i] * 1.5))
+
+    xx1.extend(df[0:8])
+    xx2.extend(df[0:8])
+
+if True:
+    fig1 = go.Figure()
+    fig1.add_trace(go.Ohlc(x=dff['Datetime'],
+                        open=dff['Open'],
+                        high=dff['High'],
+                        low=dff['Low'],
+                        close=dff['Close']))
+
+if MA1se:
+    fig1.add_trace(go.Scatter(x=dff['Datetime'], y=MA1, name='MA1'))
+if MA2se:
+    fig1.add_trace(go.Scatter(x=dff['Datetime'], y=MA2, name='MA2'))
+if Filtring:
+    fig1.add_trace(go.Scatter(x=dff['Datetime'], y=MA3, name='MA3'))
+if ATRse:
+    fig1.add_scatter(x=dff['Datetime'], y=xx1, line=dict(color='red', width=1), name='ATR Up')
+    fig1.add_scatter(x=dff['Datetime'], y=xx2, line=dict(color='green', width=1), name='ATR Down')
+if Stse:
+    fig1.add_scatter(x=dffull.index, y=df_st[0], line=dict(color='red', width=1), name='Super Trend Up')
+    fig1.add_scatter(x=dffull.index, y=df_st[2], line=dict(color='green', width=1), name='Super Trend down')
+    if EtichetteST:
+        fig1.add_trace(go.Scatter(
+            x=dffull.index,
+            y=buy_price,
+            mode="markers+text",
+            name="Buy Entris",
+            text='SELL',
+            textposition="top center"))
+        fig1.add_trace(go.Scatter(
+            x=dffull.index,
+            y=sell_price,
+            mode="markers+text",
+            name="Sell Entries",
+            text='BUY',
+            textposition="top center"))
+if TMAse:
+    fig1.add_scatter(x=dff['Datetime'], y=TMAUP, line=dict(color='black', width=1), name='TMA Up')
+    fig1.add_scatter(x=dff['Datetime'], y=TMADOWN, line=dict(color='black', width=1), name='TMA Down')
+
+fig1.update_xaxes(fixedrange=False)
+fig1.update_yaxes(fixedrange=False)
+
+stl.plotly_chart(fig1)
+
+
